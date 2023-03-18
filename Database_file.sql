112b0a94b0bf0f6040512cb7c87d562dd9e6025a
Naman Shival <126947460+namanshival123@users.noreply.github.com>
2023-03-18 14:24:57 +0530

    Update Database_file.sql

--- Database_file.sql
+++ Database_file.sql
@@ -1,8 +1,8 @@
-create database boutique;
+create database IF NOT EXISTS boutique;
 use boutique;
-create table customer(cust_id int primary key,cust_name varchar(20),last_name varchar(20),cust_contact varchar(20),cust_adrs varchar(50),booked_prod varchar(20));
+create table IF NOT EXISTS customer(cust_id int primary key,cust_name varchar(20),last_name varchar(20),cust_contact varchar(20),cust_adrs varchar(50),booked_prod varchar(20));
 desc customer;
-create table employee(emp_id varchar(5) primary key,emp_name varchar(20),emp_Lname varchar(20),emp_contact varchar(20),emp_adrs varchar(50));
+create table IF NOT EXISTS  employee(emp_id varchar(5) primary key,emp_name varchar(20),emp_Lname varchar(20),emp_contact varchar(20),emp_adrs varchar(50));
 desc employee;
-create table product(prod_no varchar(10),prod_id varchar(20) primary key,prod_price int,prod_stock int);
+create table IF NOT EXISTS product(prod_no varchar(10),prod_id varchar(20) primary key,prod_price int,prod_stock int);
 desc product;
